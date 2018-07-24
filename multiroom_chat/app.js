831ebe5b7a5b8137a5cf7fe7031f041c54711032
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-07-24 18:25:22 -0300

    Projeto pratico - Multiroom Chat real-time com WebSocket

---
+++ multiroom_chat/app.js
@@ -0,0 +1,9 @@
+/* importar as configurações do servidor */
+var app = require('./config/server');
+
+/* parametrizar a porta de escuta */
+var server = app.listen(80, function(){
+	console.log('Servidor online');
+})
+
+require('socket.io').listen(server);
