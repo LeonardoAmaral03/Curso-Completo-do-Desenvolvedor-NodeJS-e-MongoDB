b0e7034f66f0639697ab463ae17e3a8de7ddcce3
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-08-07 18:59:08 -0300

    Projeto MMORPG GOT - Criando acoes do jogo

--- mmorpg_got/app/controllers/jogo.js
+++ mmorpg_got/app/controllers/jogo.js
@@ -1,11 +1,18 @@
 module.exports.jogo = function(application, req, res){
 	res.header('Cache-control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
 
-	if(req.session.autorizado){
-		res.render('jogo');
-	} else {
+	if(req.session.autorizado !== true){
 		res.send('Usuário precisa fazer login');
+		return;
 	}
+
+	var usuario = req.session.usuario;
+	var casa = req.session.casa;
+
+	var connection = application.config.dbConnection; // módulo que exporta a função de conexão com o banco
+	var JogoDAO = new application.app.models.JogoDAO(connection); // objeto JogoDAO
+
+	JogoDAO.iniciaJogo(res, usuario, casa);
 }
 
 module.exports.sair = function(application, req, res){
@@ -13,4 +20,12 @@ module.exports.sair = function(application, req, res){
 	req.session.destroy( function(err){
 		res.redirect('/');
 	});
+}
+
+module.exports.suditos = function(application, req, res){
+	res.render("aldeoes", {validacao : {}})
+}
+
+module.exports.pergaminhos = function(application, req, res){
+	res.render("pergaminhos", {validacao : {}})
 }
--- mmorpg_got/app/routes/jogo.js
+++ mmorpg_got/app/routes/jogo.js
@@ -6,4 +6,12 @@ module.exports = function(application){
 	application.get('/sair', function(req, res){
 		application.app.controllers.jogo.sair(application, req, res);
 	});
+
+	application.get('/suditos', function(req, res){
+		application.app.controllers.jogo.suditos(application, req, res); // as rotas estao executando as propriedades do nosso controller
+	});
+
+	application.get('/pergaminhos', function(req, res){
+		application.app.controllers.jogo.pergaminhos(application, req, res);
+	});
 }
